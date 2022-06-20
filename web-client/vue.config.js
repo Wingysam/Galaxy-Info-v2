50822f188ec95792ccdf998e2f841e22fd151388
Wingy <git@wingysam.xyz>
2022-06-19 22:21:44 -0400

    client shared libs

--- web-client/vue.config.js
+++ web-client/vue.config.js
@@ -1,13 +1,22 @@
+const path = require('path')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@galaxyinfo': path.resolve(__dirname, '../shared')
+      }
+    }
+  },
   chainWebpack: config => {
     config
-        .plugin('html')
-        .tap(args => {
-            args[0].title = "Galaxy Info";
-            return args;
-        })
-}
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "Galaxy Info";
+        return args;
+      })
+  }
 }
