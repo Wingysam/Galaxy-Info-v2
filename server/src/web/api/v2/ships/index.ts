395ed0d787c63837b99a86fd6251fd5cc34cc557
Wingy <git@wingysam.xyz>
2022-06-28 13:11:16 -0400

    remove get ship api

--- server/src/web/api/v2/ships/index.ts
+++ server/src/web/api/v2/ships/index.ts
@@ -1,4 +1,3 @@
-import { serialize } from '@galaxyinfo/serialization'
 import { Router, json } from 'express'
 import { scope } from '../../../middleware/scope'
 type Arg = {
@@ -7,20 +6,6 @@ type Arg = {
 
 export async function ships ({ GalaxyInfo }: Arg) {
   const router = Router()
-
-  router.get('/', scope('ships_read'), async (req, res) => {
-    try {
-      if (typeof req.query.ship == 'string') {
-        const ship = GalaxyInfo.ships.find(req.query.ship)
-        res.send(serialize(ship))
-        return
-      }
-    } catch (error) {
-      res.send(`${error}`)
-      return
-    }
-    res.send(serialize(GalaxyInfo.ships.all({ secret: false })))
-  })
 
   router.post('/', scope('ships_write'), json({ limit: '50mb' }), async (req, res) => {
     try {
