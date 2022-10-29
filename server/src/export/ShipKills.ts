555eb92fd43d6fd875a6d39e8cbd4c50c64ac1df
Wingy <git@wingysam.xyz>
2022-10-29 00:54:54 -0400

    remove default kill log classes

--- server/src/export/ShipKills.ts
+++ server/src/export/ShipKills.ts
@@ -86,14 +86,14 @@ export class ShipKillsExport {
         const shouldEmbed = channelRow.kill_log_embed ?? this.GalaxyInfo.guildConfigs.defaults.channel.kill_log_embed
 
         if (isAll || killerIsMember || killerIsCustom) {
-          if (isLimited && !(channelRow.kill_log_limited_kill_classes ?? this.GalaxyInfo.guildConfigs.defaults.channel.kill_log_limited_kill_classes)?.includes(kill.victim_class)) continue
-          if (!isLimited && !(channelRow.kill_log_bm_kill_classes ?? this.GalaxyInfo.guildConfigs.defaults.channel.kill_log_bm_kill_classes)?.includes(kill.victim_class)) continue
+          if (isLimited && !channelRow.kill_log_limited_kill_classes?.includes(kill.victim_class)) continue
+          if (!isLimited && !channelRow.kill_log_bm_kill_classes?.includes(kill.victim_class)) continue
           try {
             await channel.send(await generateMessageBody(true, shouldEmbed, kill.nuke ? templateNuke : templateNormal))
           } catch {} // no perms, discord outage, etc
         } else if (victimIsMember || victimIsCustom) {
-          if (isLimited && !(channelRow.kill_log_limited_death_classes ?? this.GalaxyInfo.guildConfigs.defaults.channel.kill_log_limited_death_classes)?.includes(kill.victim_class)) continue
-          if (!isLimited && !(channelRow.kill_log_bm_death_classes ?? this.GalaxyInfo.guildConfigs.defaults.channel.kill_log_bm_death_classes)?.includes(kill.victim_class)) continue
+          if (isLimited && !channelRow.kill_log_limited_death_classes?.includes(kill.victim_class)) continue
+          if (!isLimited && !channelRow.kill_log_bm_death_classes?.includes(kill.victim_class)) continue
           try {
             await channel.send(await generateMessageBody(false, shouldEmbed, kill.nuke ? templateNuke : templateNormal))
           } catch {}
