522c81be7d229ce4221e03f553eadb5d379b2e37
Wingy <git@wingysam.xyz>
2023-01-12 00:01:07 -0500

    add dekneallifier

--- server/src/commands/kneall.ts
+++ server/src/commands/kneall.ts
@@ -14,12 +14,27 @@ export class KneallTranslatorCommand extends GalaxyInfoCommand {
         .setDescription('The text to translate.')
         .setRequired(true)
       )
+      .addBooleanOption(option => option
+        .setName('reverse')
+        .setDescription('Translates the message back to text')
+      )
       .setDescription('Translate a message to kneall')
     super({ builder, preconditions: [GameDevOnly], instant: true })
   }
 
   public async interactionCreate(interaction: CommandInteraction, ephemeral: boolean) {
     const text = interaction.options.getString('text', true)
+    const reverse = interaction.options.getBoolean('reverse') ?? false
+
+    if (reverse) {
+      let output = text
+      for (const [letter, emoji] of Object.entries(EMOJIS.kneall)) {
+        output = output.replaceAll(emoji, letter)
+      }
+      await interaction.reply({ ephemeral, content: output })
+      return
+    }
+
     const translated = text.toLowerCase().split('').map(letter => (EMOJIS.kneall as {[key: string]: string})[letter] ?? letter).join('')
     await interaction.reply({ ephemeral, content: translated })
   }
