0e6e3337156758a6b7067605b686d2f384279bb2
Wingy <git@wingysam.xyz>
2023-05-29 17:27:34 -0400

    frontend logged in optional error catch

--- server/src/web/middleware/frontendLoggedIn.ts
+++ server/src/web/middleware/frontendLoggedIn.ts
@@ -3,23 +3,30 @@ import fetch from 'node-fetch'
 
 export default function frontendLoggedIn (options: { optional?: boolean } = {}) {
   return async function (req: Request, res: Response, next: NextFunction) {
-    function notLoggedIn(reason: string) {
-      if (options.optional) return next()
-      return res.send(reason)
-    }
-    const token = req.headers['x-discord-token']
-    if (!token) return notLoggedIn('no token')
-    if (typeof token !== 'string') return notLoggedIn('token not a string')
-
-    const userData = await (await fetch('https://discord.com/api/v9/users/@me', {
-      headers: {
-        Authorization: 'Bearer ' + token
+    try {
+      function notLoggedIn(reason: string) {
+        if (options.optional) return next()
+        return res.send(reason)
       }
-    })).json()
-    if (userData.errors) return notLoggedIn('discord api returned an error: ' + JSON.stringify(userData.errors))
-
-    userData._token = token
-    req.discordUser = userData
-    return next()
+      const token = req.headers['x-discord-token']
+      if (!token) return notLoggedIn('no token')
+      if (typeof token !== 'string') return notLoggedIn('token not a string')
+  
+      const userData = await (await fetch('https://discord.com/api/v9/users/@me', {
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
+      })).json()
+      if (userData.errors) return notLoggedIn('discord api returned an error: ' + JSON.stringify(userData.errors))
+  
+      userData._token = token
+      req.discordUser = userData
+      return next()
+    } catch (error) {
+      if (options.optional) {
+        return next()
+      }
+      throw error
+    }
   }
 }
